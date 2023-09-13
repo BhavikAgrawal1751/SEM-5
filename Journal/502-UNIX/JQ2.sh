20b65f41a2c08e42ebd3f7d22e4c1a30399b3f5e
Bhavik Agrawal <102372077+BhavikAgrawal1751@users.noreply.github.com>
2023-09-13 08:30:31 +0530

    Add files via upload

--- Journal/502-UNIX/JQ2.sh
+++ Journal/502-UNIX/JQ2.sh
@@ -1,12 +1,15 @@
-if [ $# -ne 2 ] ; then
+if [ $# -ne 2 ]  
+then
 	echo "No Argument"
-elif [ ! -e $1 -o ! -e $2 ] ; then
+elif [ ! -e $1 -o ! -e $2 ] 
+then
 	echo "File does not exists"
 else
-	p1=`ls -L $1 | cut - c2 - 10 `
-	p2=`ls -L $2 | cut -c2 -10`
-if [ $p1 == $p2 ] ; then
-	echo "file permission are equal is $p1"
+	p1=`ls -l $1 | cut -c2-10`
+	p2=`ls -l $2 | cut -c2-10`
+if [ $p1 = $p2 ] 
+then
+	echo "file permission are equal & ls $p1"
 else 
 	echo "file permission are not equal"
 	echo "1st file $p1"
